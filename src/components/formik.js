68a74b5d12160266fd607b6b0c0f58079a0a601f
simooooone <s.foschi@protonmail.com>
2020-12-26 12:23:14 +0100

    Updated form

--- src/components/formik.js
+++ src/components/formik.js
@@ -38,17 +38,17 @@ const FormikContact = () => {
       }}
       onSubmit={(values, actions) => {
         emailjs
-          .sendForm("service_q3997uk", "template_m6tzcmm", "contact_form")
-          .then(() => {
-            console.log("form spedito")
+          .sendForm("service_q3997uk", "template_m6tzcmm", "#contact_form")
+          .then(result => {
+            console.log(result.text, result.status)
           })
-          .catch(() => {
-            console.log("Invio form fallito")
+          .catch(error => {
+            console.log(error.text)
             return
           })
         actions.resetForm()
         actions.setSubmitting(false)
-        document.location.assign("/thanks")
+        //document.location.assign("/thanks")
       }}
       validationSchema={validationSchema}
     >
@@ -117,7 +117,7 @@ const FormikContact = () => {
               Invia
             </Button>
           </div>
-          {/* <pre>{JSON.stringify(props.values, null, 2)}</pre>
+          {/*<pre>{JSON.stringify(props.values, null, 2)}</pre>
           <pre>{JSON.stringify(props.errors, null, 2)}</pre> */}
         </Form>
       )}
